9420cc5d06f2606b0950b7ec80aafc1973596907
Manura <w.m.manurasanjula2003@outlook.com>
2023-04-12 23:17:32 +0530

    Textures Added 12/04/2023 23:17

--- OpenGlCourseApp1/Shaders/shader.frag
+++ OpenGlCourseApp1/Shaders/shader.frag
@@ -1,10 +1,12 @@
 #version 330
 
 in vec4 vCol;
-
+in vec2 TextCoord;
 out vec4 colour;
+
+uniform sampler2D theTexture;
 
 void main()
 {
-	colour = vCol;
+	colour = texture(theTexture, TextCoord);
 }
---
+++ OpenGlCourseApp1/Texture.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <GL\glew.h>
+#include <string>
+#include "stb_image.h"
+
+class Texture
+{
+public:
+	Texture();
+	Texture(std::string fileLoc);
+
+	void LoadTexture();
+	void UseTexture();
+	void ClearTexture();
+
+	~Texture();
+
+private:
+	GLuint textureID;
+	int width, height, bitDepth;
+	std::string fileLocation;
+};
+
